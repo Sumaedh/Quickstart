cb394dc0839d58240ce378c7062e04ec48dd4642
Sumaedh <sumaedhk1@gmail.com>
2026-03-01 17:39:29 -0500

    updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensorTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -16,11 +17,15 @@ public class DistanceSensorTest extends OpMode {
 
     public static double BALL_DETECT_THRESHOLD_INCHES = 1.0;
 
+    private FtcDashboard dashboard;
+
     @Override
     public void init() {
         distanceSensor1 = hardwareMap.get(DistanceSensor.class, "distanceSensor1");
         distanceSensor2 = hardwareMap.get(DistanceSensor.class, "distanceSensor2");
         distanceSensor3 = hardwareMap.get(DistanceSensor.class, "distanceSensor3");
+
+        dashboard = FtcDashboard.getInstance();
 
         telemetry.addLine("All 3 Distance Sensors Initialized");
         telemetry.update();
@@ -29,19 +34,24 @@ public class DistanceSensorTest extends OpMode {
     @Override
     public void loop() {
 
-        displaySensor("Sensor 1", distanceSensor1);
-        displaySensor("Sensor 2", distanceSensor2);
-        displaySensor("Sensor 3", distanceSensor3);
+        sendSensorData("Sensor 1", distanceSensor1);
+        sendSensorData("Sensor 2", distanceSensor2);
+        sendSensorData("Sensor 3", distanceSensor3);
 
         telemetry.update();
+        dashboard.getTelemetry().update();
     }
 
-    private void displaySensor(String name, DistanceSensor sensor) {
+    private void sendSensorData(String name, DistanceSensor sensor) {
         double distInches = sensor.getDistance(DistanceUnit.INCH);
         boolean ballDetected = distInches > 0 && distInches < BALL_DETECT_THRESHOLD_INCHES;
 
         telemetry.addLine(name);
         telemetry.addData("  Distance (in)", distInches);
         telemetry.addData("  Ball Detected", ballDetected);
+
+        dashboard.getTelemetry().addLine(name);
+        dashboard.getTelemetry().addData("  Distance (in)", distInches);
+        dashboard.getTelemetry().addData("  Ball Detected", ballDetected);
     }
 }
